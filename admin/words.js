df16a6e2be84a64b617dfea00f8318c34a152e82
root <root@debbierack.traumhaus>
2019-07-20 16:29:38 +0200

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"allnetipsocketoutlet4176 adapter settings": {
+		"en": "Adapter settings for allnetipsocketoutlet4176",
+		"de": "Adaptereinstellungen für allnetipsocketoutlet4176",
+		"ru": "Настройки адаптера для allnetipsocketoutlet4176",
+		"pt": "Configurações do adaptador para allnetipsocketoutlet4176",
+		"nl": "Adapterinstellingen voor allnetipsocketoutlet4176",
+		"fr": "Paramètres d'adaptateur pour allnetipsocketoutlet4176",
+		"it": "Impostazioni dell'adattatore per allnetipsocketoutlet4176",
+		"es": "Ajustes del adaptador para allnetipsocketoutlet4176",
+		"pl": "Ustawienia adaptera dla allnetipsocketoutlet4176",
+		"zh-cn": "allnetipsocketoutlet4176的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
